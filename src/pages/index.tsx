c2c7cbd0aa8e121384c5f24ee598dd445dda0685
takayasu <a.takayasu@gmail.com>
2023-10-04 18:05:50 +0900

    出題用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import SearchUser from '@/components/organism/SearchUser'
-import SearchUserBox from '@/components/moleculous/UserSearchBox'
 
 export default function Home() {
   const [user, setUser] = React.useState('takayasu')
 
-  const changeUser = (user: string) => {
-    console.log(user);
-    setUser(user);
-  }
-
   return (
     <div className="m-8 flex flex-col items-center">
-      <SearchUserBox setSearchUser={changeUser} />
       <SearchUser user={user}/>
 
     </div>)
